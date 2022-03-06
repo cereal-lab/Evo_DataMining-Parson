361556e8c18a686095a78c35841420a4194bd67d
TerryTkc <terencetkc@hotmail.com>
2022-03-06 16:47:40 -0500

    R - added more plots

--- Visualization.R
+++ Visualization.R
@@ -8,6 +8,8 @@ library("lmtest")
 library("MASS")
 library("rsq")
 
+df_new <- read.csv("Puzzles_Output/best_final_output.csv")
+
 virtualization_seq_freq <- function(df){
 
 cl1.3 <- df$Clusters
@@ -27,19 +29,24 @@ cpal(data_1_E.seq) <- c("#7FC97F", "#BEAED4", "#FDC086", "#FFFF99", "#386CB0", "
 
 #idxs used to find interval representing the range to be plotted.
 seqlegend(data_1_E.seq) 
-seqiplot(data_1_E.seq, with.legend=F, main = "First 10 Sequence" ) 
+# seqiplot(data_1_E.seq, with.legend=F, main = "First 10 Sequence" ) 
 seqfplot(data_1_E.seq, pbarw=T, with.legend=F, main = "Most Frequent Sequences") 
-seqdplot(data_1_E.seq, with.legend=F,  main = "State Distribution") 
 
 cl1.3fac <- factor(cl1.3, labels = type_label)
 # 
 # seqdplot(data_1_E.seq, group=cl1.3fac,main = "Type State Distribution", border=NA)
 seqfplot(data_1_E.seq, group=cl1.3fac, pbarw=T, main = "Most Frequent Sequences")
 
+### More options: http://traminer.unige.ch/doc/seqplot.html
+seqdplot(data_1_E.seq, group=cl1.3fac, main = "Type State Distribution")
+seqIplot(data_1_E.seq, group=cl1.3fac, main = "Whole Set Index Plot")
+seqHtplot(data_1_E.seq, group=cl1.3fac, main = "Entropy")
+seqpcplot(data_1_E.seq, group=cl1.3fac, main = "Parallel Coordinate Plot")
+
 }
 
 virtualization_seq_dist <- function(df){
-  
+
   cl1.3 <- df$Clusters
   max.cluster <- max(cl1.3)
   temp_2 <- strsplit(df$final_sequence, "")
@@ -47,34 +54,47 @@ virtualization_seq_dist <- function(df){
   max.length <- max(sapply(temp_2, length))
   temp_2 <- lapply(temp_2, function(v) { c(v, rep(NA, max.length-length(v)))})
   temp <- data.frame(do.call(rbind, temp_2))
-  
+
   temp.alphabet <- c("T","P","O","I","V","F","E", "R", "X");
   temp.labels <- c("Trash","Braces","Output","Input","Variable","If","Else", "Reorder", "Padding")
   temp.scodes <- c("T","P","O","I","V","F","E", "R", "X")
   data_1_E.seq <- seqdef(temp, 0:max.length, alphabet = temp.alphabet, states=temp.scodes, labels=temp.labels)
-  
+
   cpal(data_1_E.seq) <- c("#7FC97F", "#BEAED4", "#FDC086", "#FFFF99", "#386CB0", "#F0027F", "#BF5B17", "#666666", "white")
   
   #idxs used to find interval representing the range to be plotted.
   # seqlegend(data_1_E.seq) 
-  # seqiplot(data_1_E.seq, with.legend=F, main = "First 10 Sequence" ) 
-  # seqfplot(data_1_E.seq, pbarw=T, with.legend=F, main = "Most Frequent Sequences") 
-  # seqdplot(data_1_E.seq, with.legend=F,  main = "State Distribution") 
+  seqdplot(data_1_E.seq, with.legend=F,  main = "State Distribution") 
+
+  ## Get state freq with seqmeant
+  mt <- seqmeant(data_1_E.seq)
+  ## order of frequencies
+  ord <- order(mt, decreasing = TRUE)
+  
+  ## Sorted alphabet
+  alph.s <- rownames(mt)[ord]
+  ## we need also to sort accordingly labels and colors
+  mvad.labels.s <- temp.labels[ord]
+  mvad.scode.s <- temp.scodes[ord]
+  cpal.s <- cpal(data_1_E.seq)[ord]
+  
+  ## Define sequence object with sorted states
+  mvad.seq.s <- seqdef(data_1_E.seq, alphabet = alph.s, states = mvad.scode.s,
+                       labels = mvad.labels.s, cpal = cpal.s, xtstep = 6)
   
   cl1.3fac <- factor(cl1.3, labels = type_label)
   
-  seqdplot(data_1_E.seq, group=cl1.3fac,main = "Type State Distribution", border=NA)
-  # seqfplot(data_1_E.seq, group=cl1.3fac, pbarw=T, main = "Most Frequent Sequences")
-  
+  seqdplot(mvad.seq.s, group=cl1.3fac, main = "State distribution plot")
+
 }
 
 par(mfrow=c(2,2))
-df_new <- read.csv("Puzzles_Output/standard_best_final_output.csv")
 df_new$new_sequence <- as.character(df_new$new_sequence)
 df_new$final_sequence <- as.character(df_new$new_sequence)
 
 max_char <- max(nchar(df_new$final_sequence))
 
+### Adding padding into sequences for better distribution visualization
 for(x in 1:nrow(df_new)){
   padding <- 0
   seq_len <- nchar(df_new$final_sequence[x])
@@ -88,8 +108,36 @@ for(x in 1:nrow(df_new)){
 }
 
 virtualization_seq_freq(df_new)
+dev.new()
 virtualization_seq_dist(df_new)
 
+### Original 
+# virtualization_seq_dist <- function(df){
+#   
+#   cl1.3 <- df$Clusters
+#   max.cluster <- max(cl1.3)
+#   temp_2 <- strsplit(df$final_sequence, "")
+#   type_label <- sprintf("Type %d",1:max.cluster)
+#   max.length <- max(sapply(temp_2, length))
+#   temp_2 <- lapply(temp_2, function(v) { c(v, rep(NA, max.length-length(v)))})
+#   temp <- data.frame(do.call(rbind, temp_2))
+#   
+#   temp.alphabet <- c("T","P","O","I","V","F","E", "R", "X");
+#   temp.labels <- c("Trash","Braces","Output","Input","Variable","If","Else", "Reorder", "Padding")
+#   temp.scodes <- c("T","P","O","I","V","F","E", "R", "X")
+#   data_1_E.seq <- seqdef(temp, 0:max.length, alphabet = temp.alphabet, states=temp.scodes, labels=temp.labels)
+#   
+#   cpal(data_1_E.seq) <- c("#7FC97F", "#BEAED4", "#FDC086", "#FFFF99", "#386CB0", "#F0027F", "#BF5B17", "#666666", "white")
+#   
+#   cl1.3fac <- factor(cl1.3, labels = type_label)
+#   
+#   ### More options: 
+#   seqdplot(data_1_E.seq, group=cl1.3fac, main = "Type State Distribution")
+#   seqIplot(data_1_E.seq, group=cl1.3fac, main = "Whole Set Index Plot")
+#   seqHtplot(data_1_E.seq, group=cl1.3fac, main = "Entropy")
+#   seqpcplot(data_1_E.seq, group=cl1.3fac, main = "Parallel Coordinate Plot")
+#   
+# }
 
 
 
